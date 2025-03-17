d96b9e2915dbcbcefa78ff558412d3120ff25694
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2025-03-17 13:35:41 +0900

    Update daumAddressScript.js

--- jspMVCHKShopping/src/main/webapp/js/daumAddressScript.js
+++ jspMVCHKShopping/src/main/webapp/js/daumAddressScript.js
@@ -1,5 +1,7 @@
 /**
  * daumAddressScript.js
+ * <script src="https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
+ * <script type="text/javascript" src ="js/daumAddressScript.js"></script>
  */
 function execDaumPostcode() {
    new daum.Postcode({
